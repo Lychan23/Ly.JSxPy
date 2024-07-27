aea0834823d1bdbd1a8d2da9154550ccb331fb5d
Lychan23 <tester.yap84@gmail.com>
2024-07-27 08:05:08 +0700

    source download test 0.3

--- app/download/page.tsx
+++ app/download/page.tsx
@@ -1,5 +1,4 @@
-// src/DownloadPage.tsx
-"use client"
+"use client";
 import React from 'react';
 
 const DownloadPage: React.FC = () => {
@@ -9,7 +8,7 @@ const DownloadPage: React.FC = () => {
       <p className="text-lg mb-4">Click the buttons below to download the files you need.</p>
       <div className="flex space-x-4">
         <a
-          href="https://cdn.discordapp.com/attachments/1259815698197123215/1261220934933676042/configure.bat?ex=66922b07&is=6690d987&hm=461262451143f62604fe059d4628c3511ad46665dd4a77ac07979afdfeaa3280&"
+          href="/downloads/installer.exe"
           download
           className="py-2 px-4 rounded text-white font-bold animate-neon"
           style={{
@@ -17,10 +16,10 @@ const DownloadPage: React.FC = () => {
             backgroundSize: '400% 400%',
           }}
         >
-          Download File 1
+          Download Installer for Windows
         </a>
         <a
-          href="https://cdn.discordapp.com/path/to/your/file2.zip"
+          href="/downloads/installer_0.1.0_arm64.deb"
           download
           className="py-2 px-4 rounded text-white font-bold animate-neon"
           style={{
@@ -28,7 +27,18 @@ const DownloadPage: React.FC = () => {
             backgroundSize: '400% 400%',
           }}
         >
-          Download File 2
+          Download ARM64 Debian Package
+        </a>
+        <a
+          href="/downloads/installer_0.1.0_amd64.deb"
+          download
+          className="py-2 px-4 rounded text-white font-bold animate-neon"
+          style={{
+            background: 'linear-gradient(270deg, #ff8a00, #e52e71, #9b4bff)',
+            backgroundSize: '400% 400%',
+          }}
+        >
+          Download AMD64 Debian Package
         </a>
       </div>
     </div>
